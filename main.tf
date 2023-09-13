c616b86b3d16a0f16b9292079f614ee35aa6aa55
Gabriel Gama <groukdev@gmail.com>
2023-09-13 09:15:13 +0100

    add s3 backend and some resources

--- main.tf
+++ main.tf
@@ -3,10 +3,50 @@ terraform {
   required_providers {
     aws = ">= 4.0"
   }
+
+  backend "s3" {
+    bucket = "groukdev-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
-  profile                  = "default"
-  shared_credentials_files = "~/.aws/credentials"
-  region                   = "us-east-1"
+  profile             = "default"
+  shared_config_files = ["~/.aws/credentials"]
+  region              = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3a.nano"
+
+  tags = {
+    Name = "HelloWorld2"
+  }
 }
